8385aba8887214f513370729673d7f5b2ef7752a
memm1991 <72989632+memm1991@users.noreply.github.com>
2021-09-12 22:43:51 -0300

    add post route

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { isValidObjectId } = require("mongoose");
 const Statistics = require('../models/statistics')
 
 
@@ -26,7 +27,49 @@ router.get('/:countryId',async(req,res)=>{
 })
 
 router.post('/',async(req,res)=>{
+    try {
+        const countryObject= await Statistics.findOne({country:req.body.country})
+        //here we calculate the total cases by adding new cases
+        let casesTotal;
+        let deathsTotal;
+        if(!countryObject.cases.total){
+             casesTotal= 0 + req.body.cases.new
+        }
+        else{
+             casesTotal=countryObject.cases.total + req.body.cases.new
+        }
+        //here we calculate the total deaths by adding new deaths
+        if(!countryObject.deaths.total){
+             deathsTotal= countryObject.deaths.total+ req.body.deaths.new
+        }
+        else{
+            countryObject.cases.total + req.body.deaths.new
+        }
+        
+        const{country,...info}=req.body
+        const updateData={...info,
+            cases:{...info.cases,
+                total:casesTotal
 
+        },deaths:{
+            ...info.deaths,
+            total:deathsTotal
+        }
+    }
+    const response=await Statistics.findByIdAndUpdate(
+        countryObject._id,
+        {
+          $set: updateData,
+        },
+        { new: true }
+      );
+  res.status(200).json(response)
+} catch (error) {
+    console.log(error)
+}
 })
 
 module.exports=router
+        
+        
+
